44de1b966f3f62f9fbab89a68885a23b77aac9f4
Tausifali Saiyed <tausifali.mhs@gmail.com>
2018-05-01 17:38:42 +0530

    Update mail.php

--- contact-us/mail.php
+++ contact-us/mail.php
@@ -1,10 +1,10 @@
 <?php
 // the message
-$msg = "First line of text\nSecond line of text";
-
+//$msg = "First line of text\nSecond line of text";
+echo "Hello world";
 // use wordwrap() if lines are longer than 70 characters
-$msg = wordwrap($msg,70);
+//$msg = wordwrap($msg,70);
 
 // send email
-mail("tausifali.mhs@gmail.com","My subject",$msg);
+//mail("tausifali.mhs@gmail.com","My subject",$msg);
 ?>
